81075cab8be29f3a3b5a0df27199d274d348df13
Aymeric <aymeric.loos@soprasteria.com>
2024-02-06 15:22:45 +0100

    Ajout projet test

---
+++ 1BRChallenge/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace _1BRChallenge
+{
+	public class Utils
+	{
+		public Utils()
+		{
+		}
+		public static int GetConstantResult()
+		{
+			int exampleResult = 10;
+			return exampleResult;
+		}
+	}
+}
+
---
+++ 1BRChallengeTest/UtilsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using _1BRChallenge;
+
+namespace _1BRChallengeTest
+{
+    [TestClass]
+    public class UtilsTest
+	{
+        [TestMethod]
+        public void GetConstantResultTest()
+        {
+            int result = Utils.GetConstantResult();
+            Assert.AreEqual(10, result);
+        }
+    }
+}
+
